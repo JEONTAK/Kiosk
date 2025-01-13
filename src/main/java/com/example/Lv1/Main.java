348d577d6d611f485884aad6a0d2eb3b457648a6
JEONTAK <tak980418@naver.com>
2025-01-13 15:58:34 +0900

    feat : implement Lv2

--- src/main/java/com/example/Lv1/Main.java
+++ src/main/java/com/example/Lv1/Main.java
@@ -8,6 +8,7 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        //메뉴 저장
         String[] menus = {
                 "ShackBurger   | W 6.9 | 토마토, 양상추, 쉑소스가 토핑된 치즈버거",
                 "SmokeShack    | W 8.9 | 베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거",
@@ -15,20 +16,28 @@ public class Main {
                 "Hamburger     | W 5.4 | 비프패티를 기반으로 야채가 들어간 기본버거"};
 
         while(true){
+            //메뉴판 출력
             System.out.println("[ SHAKESHACK MENU ]");
             for (int i = 0; i < menus.length; i++) {
                 System.out.println((i + 1) + ". " + menus[i]);
             }
             System.out.println("0. 종료     | 종료");
 
+            /**
+             * 메뉴 선택 수행
+             * 0 : 종료
+             * 1 ~ N : 해당 메뉴 출력
+             * N 초과 : 예외 처리
+             */
             System.out.println("\n메뉴를 선택해 주세요.");
             int selectMenu = Integer.parseInt(br.readLine());
             if(selectMenu == 0){
+                System.out.println("프로그램을 종료합니다.");
                 break;
             } else if (selectMenu <= menus.length) {
-                System.out.println("[ 선택한 메뉴는 ]");
-                System.out.println("[ " + menus[selectMenu - 1] + " ]");
-                System.out.println("[ 입니다. ]\n");
+                System.out.println("선택한 메뉴는");
+                System.out.println(menus[selectMenu - 1]);
+                System.out.println("입니다.\n");
             } else{
                 System.out.println("메뉴에 적혀있는 번호만 입력 가능합니다!\n");
             }
--- src/main/java/com/example/Lv2/model/MenuItem.java
+++ src/main/java/com/example/Lv2/model/MenuItem.java
@@ -1,4 +1,29 @@
-package com.example.Lv2;
+package com.example.Lv2.model;
 
 public class MenuItem {
+    private String menuName;
+    private double menuPrice;
+    private String menuInfo;
+
+    /**
+     * Constructor
+     *
+     * @param menuName
+     * @param menuPrice
+     * @param menuInfo
+     */
+    public MenuItem(String menuName, double menuPrice, String menuInfo) {
+        this.menuName = menuName;
+        this.menuPrice = menuPrice;
+        this.menuInfo = menuInfo;
+    }
+
+    /**
+     * Method
+     *
+     * Print current menuItem
+     */
+    public void printMenuItem() {
+        System.out.println(String.format("%-5s", menuName) + "| W " + menuPrice + " | " + menuInfo);
+    }
 }
